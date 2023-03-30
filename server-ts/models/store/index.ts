3b711725633d80a0ccb7a0e57fe3a307db115f00
Pedro Martin <pataruco@users.noreply.github.com>
2023-03-30 20:38:02 +0100

    Parse PostGIS Geography type binary value to lat and lng

--- server-ts/models/store/index.ts
+++ server-ts/models/store/index.ts
@@ -1,11 +1,49 @@
 import { query } from '../../db/index.js';
+import { Store } from '../../types/store/index.js';
+import { Geometry } from 'wkx';
 
-export const getStoreById = async (id: string) => {
-  const { rows } = await query(`SELECT * from stores WHERE store_id = $1`, [
-    id,
-  ]);
+interface StoreRow {
+  store_id: string;
+  geog: string;
+  name: string;
+  address: string;
+  created_at: Date;
+  updatedd_at: Date;
+}
 
-  return rows[0];
+interface GeoJson {
+  coordinates: [number, number];
+}
+
+export const getStoreById = async (
+  _parent: unknown,
+  { id }: { id: Store['id'] },
+) => {
+  const { rows }: { rows: StoreRow[] } = await query(
+    `SELECT * from stores WHERE store_id = '${id}'`,
+  );
+
+  const { store_id, geog, name, address, created_at, updatedd_at } = rows[0];
+
+  const { coordinates } = Geometry.parse(
+    Buffer.from(geog, 'hex'),
+  ).toGeoJSON() as GeoJson;
+
+  const [lng, lat] = coordinates;
+
+  const store: Store = {
+    id: store_id,
+    name,
+    address,
+    coordinates: {
+      lat,
+      lng,
+    },
+    createdAt: created_at,
+    updatedAt: updatedd_at,
+  };
+
+  return store;
 };
 
 // export const createStore = async ({ name, address, coordinates }: Store) => {
