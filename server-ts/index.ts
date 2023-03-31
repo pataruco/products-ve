3de3a1234878f40ffbda64c4f8d831c4d6305592
Pedro Martin <pataruco@users.noreply.github.com>
2023-03-31 17:56:25 +0100

    Set indentation to space

--- server-ts/index.ts
+++ server-ts/index.ts
@@ -21,38 +21,38 @@ const app = express();
 const httpServer = http.createServer(app);
 
 const main = async () => {
-	const typeDefs = gql(
-		readFileSync('schema.graphql', {
-			encoding: 'utf-8',
-		}),
-	);
-	// Same ApolloServer initialization as before, plus the drain plugin
-	// for our httpServer.
-	const server = new ApolloServer<Context>({
-		typeDefs,
-		resolvers,
-		plugins: [
-			ApolloServerPluginDrainHttpServer({ httpServer }),
-			apolloLogger({}),
-		],
-	});
-	// Ensure we wait for our server to start
-	await server.start();
-	// Set up our Express middleware to handle CORS, body parsing,
-	// and our expressMiddleware function.
-	app.use(
-		'/',
-		cors<cors.CorsRequest>(),
-		bodyParser.json(),
-		// expressMiddleware accepts the same arguments:
-		// an Apollo Server instance and optional configuration options
-		expressMiddleware(server, {
-			context: async ({ req }) => ({ token: req.headers.token }),
-		}),
-	);
-	// Modified server startup
+  const typeDefs = gql(
+    readFileSync('schema.graphql', {
+      encoding: 'utf-8',
+    }),
+  );
+  // Same ApolloServer initialization as before, plus the drain plugin
+  // for our httpServer.
+  const server = new ApolloServer<Context>({
+    typeDefs,
+    resolvers,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      apolloLogger({}),
+    ],
+  });
+  // Ensure we wait for our server to start
+  await server.start();
+  // Set up our Express middleware to handle CORS, body parsing,
+  // and our expressMiddleware function.
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+  // Modified server startup
 
-	await httpServer.listen({ port: PORT });
+  await httpServer.listen({ port: PORT });
 };
 
 main();
