9337cd2507d12d228a60cf30c56a43b1e8e7d9d5
Pedro Martin <pataruco@users.noreply.github.com>
2023-04-03 08:51:28 +0100

    Create products table

---
+++ server-ts/db/migrations/0002-products-table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE products (
+  product_id uuid PRIMARY KEY DEFAULT (uuid_generate_v4 ()),
+  name varchar(255) NOT NULL,
+  brand varchar(255),
+  created_at timestamp NOT NULL DEFAULT (now()),
+  updated timestamp NOT NULL DEFAULT (current_timestamp)
+);
+
+COMMENT ON TABLE stores IS 'Stores stores locations';
+
+CREATE TABLE stores_products (
+  stores_store_id uuid,
+  products_product_id uuid,
+  PRIMARY KEY (stores_store_id, products_product_id)
+);
+
+ALTER TABLE
+  stores_products
+ADD
+  FOREIGN KEY (stores_store_id) REFERENCES stores (store_id);
+
+ALTER TABLE
+  stores_products
+ADD
+  FOREIGN KEY (products_product_id) REFERENCES products (product_id);
