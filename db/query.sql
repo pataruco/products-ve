cc7cb6580794f391ca0e563d441c72c9fb95eddd
Pedro Martin <pataruco@gmail.com>
2023-04-21 15:00:30 +0100

    Remove redundant queries

--- db/query.sql
+++ db/query.sql
@@ -12,52 +12,14 @@ WHERE
   );
 
 SELECT
-  *
+  -- Avoid duplicates
+  DISTINCT ON (store_id) stores.geog,
+  stores.name,
+  stores.address,
+  stores.created_at,
+  stores.updatedd_at
 FROM
-  stores -- Product
-  JOIN products ON products.product_id = (
-    SELECT
-      products.product_id
-    FROM
-      products
-    WHERE
-      name = 'Cocosette'
-  )
-WHERE
-  ST_DWithin (
-    geog,
-    -- London
-    ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
-    -- Metres
-    3000
-  );
-
-SELECT
-  *
-FROM
-  stores -- Product
-  JOIN stores_products ON stores_products.products_product_id = (
-    SELECT
-      products.product_id
-    FROM
-      products
-    WHERE
-      reference = 'COCOSETTE'
-  )
-WHERE
-  ST_DWithin (
-    geog,
-    -- London
-    ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
-    -- Metres
-    3000
-  );
-
----
-SELECT
-  DISTINCT ON (store_id) *
-FROM
-  stores -- Product
+  stores -- Many to many by joint table
   JOIN stores_products ON stores_products.products_product_id = (
     SELECT
       products.product_id
@@ -70,8 +32,8 @@ FROM
 WHERE
   ST_DWithin (
     geog,
-    -- London
+    -- LONDON
     ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
-    -- Metres
+    -- METRES
     10000
   );
