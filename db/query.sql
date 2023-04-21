a45e89017e4e1b1aa99c9bf0c3e0411d7c03a9a7
Pedro Martin <pataruco@gmail.com>
2023-04-21 14:05:31 +0100

    Query distinct

--- db/query.sql
+++ db/query.sql
@@ -52,3 +52,39 @@ WHERE
     -- Metres
     3000
   );
+
+---
+SELECT
+  DISTINCT ON (store_id) *
+FROM
+  stores -- Product
+  JOIN stores_products ON stores_products.products_product_id = (
+    SELECT
+      products.product_id
+    FROM
+      products
+    WHERE
+      reference = 'COCOSETTE'
+  )
+WHERE
+  ST_DWithin (
+    geog,
+    -- London
+    ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
+    -- Metres
+    3000
+  );
+
+--- 
+SELECT
+  *
+FROM
+  stores
+WHERE
+  ST_DWithin(
+    geog,
+    -- London
+    ST_GeographyFromText('POINT(-0.1275 51.50722)'),
+    -- Metres
+    10000
+  );
