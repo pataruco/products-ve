4c97ef9ad7ef3b528085385596b69a30aa47d960
Pedro Martin <pataruco@gmail.com>
2023-04-27 10:17:09 +0100

    Set config

--- projects/server/config/google-auth.ts
+++ projects/server/config/google-auth.ts
@@ -1,40 +1,35 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const { authenticate } = require('@google-cloud/local-auth');
-const { google } = require('googleapis');
+import fs from 'fs/promises';
+import path from 'path';
+import process from 'process';
+
+import { authenticate } from '@google-cloud/local-auth';
+import type { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), '/config', 'token.json');
+const CREDENTIALS_PATH = path.join(
+  process.cwd(),
+  '/config',
+  'credentials.json',
+);
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
 async function loadSavedCredentialsIfExist() {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
+    const content = (await fs.readFile(TOKEN_PATH)).toString();
     const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
+    return google.auth.fromJSON(credentials) as OAuth2Client;
   } catch (err) {
     return null;
   }
 }
 
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
+async function saveCredentials(client: OAuth2Client) {
+  const content = (await fs.readFile(CREDENTIALS_PATH)).toString();
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
@@ -46,10 +41,6 @@ async function saveCredentials(client) {
   await fs.writeFile(TOKEN_PATH, payload);
 }
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
 async function authorize() {
   let client = await loadSavedCredentialsIfExist();
   if (client) {
@@ -59,18 +50,14 @@ async function authorize() {
     scopes: SCOPES,
     keyfilePath: CREDENTIALS_PATH,
   });
+
   if (client.credentials) {
     await saveCredentials(client);
   }
   return client;
 }
 
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listLabels(auth) {
+async function listLabels(auth: OAuth2Client) {
   const gmail = google.gmail({ version: 'v1', auth });
   const res = await gmail.users.labels.list({
     userId: 'me',
---
+++ projects/server/config/refresh-token.ts
@@ -0,0 +1,4 @@
+const CLIENT_ID =
+  '947337672995-c3j7breqlbg2s05e8atvrmost7eejviq.apps.googleusercontent.com';
+
+const CLIENT_SECRET = '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';
