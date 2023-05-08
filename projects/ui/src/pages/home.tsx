4b7063e2a6955c23b4a59df162a1fa79a3adc783
Pedro Martin <pataruco@gmail.com>
2023-05-08 15:56:02 +0100

    Rendering a map with markers

--- projects/ui/src/pages/home.tsx
+++ projects/ui/src/pages/home.tsx
@@ -1,3 +1,5 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 import { useStoresQuery } from '../api/operations.generated';
 
 const Home = () => {
@@ -8,16 +10,53 @@ const Home = () => {
           lat: 51.50722,
           lng: -0.1275,
         },
-        distance: 2500,
+        distance: 10000,
       },
     },
   });
 
-  console.log({ data });
-
   if (loading) return <p>Loading...</p>;
 
   if (error) return <p>Error : {error.message}</p>;
+
+  const markers = data?.stores?.map(
+    ({ id, coordinates: { lat, lng }, name, address }) => {
+      return (
+        <Marker position={[lat, lng]} key={id}>
+          <Popup>
+            <p>{name}</p>
+            <address>{address}</address>
+          </Popup>
+        </Marker>
+      );
+    },
+  );
+
+  return (
+    <MapContainer
+      // @ts-ignore bad TS import on react-leaflet
+      center={[51.50722, -0.1275]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        // @ts-ignore bad TS import on react-leaflet
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {data?.stores?.map(({ id, coordinates: { lat, lng }, name, address }) => {
+        return (
+          <Marker position={[lat, lng]} key={id}>
+            <Popup>
+              <p>{name}</p>
+              <address>{address}</address>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
 };
 
 export default Home;
