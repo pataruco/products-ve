8d23417b6e3249f843da332f389a17b4e04c7835
Pedro Martin <pataruco@gmail.com>
2023-05-08 16:56:24 +0100

    Change tile render engine

--- projects/ui/src/pages/home.tsx
+++ projects/ui/src/pages/home.tsx
@@ -44,7 +44,8 @@ const Home = () => {
       <TileLayer
         // @ts-ignore bad TS import on react-leaflet
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}@2x.png?key=Afwd8yuPeMViLnCdQzLO"
       />
 
       {markers ? [...markers] : null}
