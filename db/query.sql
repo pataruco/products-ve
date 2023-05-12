aced217a566974ba03fdce5f710066cd086dad1d
Pedro Martin <pataruco@gmail.com>
2023-05-12 12:53:59 +0100

    Update SQL query reference

--- db/query.sql
+++ db/query.sql
@@ -12,7 +12,7 @@ WHERE
     10000
   );
 
--- Get stores within a radius from a point with products available as JSON array
+-- Get stores within a radius from a point with products available as an array
 SELECT
   stores.store_id,
   stores.name,
@@ -23,7 +23,7 @@ SELECT
   ARRAY_AGG(
     JSON_BUILD_OBJECT(
       'name',
-      products.name,
+      products.reference,
       'id',
       products.product_id,
       'brand',
@@ -49,159 +49,70 @@ WHERE
 GROUP BY
   stores.store_id;
 
--- Get stores within a radius from a point and product name
+-- Get stores within a radius from a point where product is with products available as an array
 SELECT
-  -- Avoid duplicates
-  DISTINCT ON (store_id) stores.geog,
+  stores.store_id,
   stores.name,
   stores.address,
+  stores.geog,
   stores.created_at,
-  stores.updated_at
+  stores.updated_at,
+  ARRAY_AGG(
+    JSON_BUILD_OBJECT(
+      'name',
+      products.reference,
+      'id',
+      products.product_id,
+      'brand',
+      products.brand,
+      'createdAt',
+      products.created_at,
+      'updatedAt',
+      products.updated
+    )
+  ) AS products
 FROM
-  stores -- Many to many by joint table
-  JOIN stores_products ON stores_products.products_product_id = (
-    SELECT
-      products.product_id
-    FROM
-      products
-    WHERE
-      reference = 'COCOSETTE'
-  )
-  JOIN products ON stores_products.stores_store_id = store_id
+  stores
+  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
+  JOIN products ON stores_products.products_product_id = products.product_id
 WHERE
-  ST_DWithin (
+  products.reference = '<PRODUCT REFERENCE>' ST_DWithin (
     geog,
     -- LONDON
     ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
     -- METRES
     10000
-  );
-
--- Get store with products as JSON array
-SELECT
-  stores.store_id,
-  stores.name,
-  stores.address,
-  stores.geog,
-  stores.created_at,
-  stores.updated_at,
-  ARRAY_AGG(
-    JSON_BUILD_OBJECT(
-      'name',
-      products.name,
-      'id',
-      products.product_id,
-      'brand',
-      products.brand,
-      'createdAt',
-      products.created_at,
-      'updatedAt',
-      products.updated
-    )
-  ) products
-FROM
-  stores
-  JOIN stores_products ON stores_products.stores_store_id = '<STORE_ID>'
-  JOIN products ON stores_products.stores_store_id = '<STORE_ID>'
-WHERE
-  stores.store_id = '<STORE_ID>'
-GROUP BY
-  stores.store_id;
-
--- Select store by ID and returns an array of products
-SELECT
-  stores.store_id,
-  stores.name,
-  stores.address,
-  stores.geog,
-  stores.created_at,
-  stores.updated_at,
-  ARRAY_AGG(
-    JSON_BUILD_OBJECT(
-      'name',
-      products.name,
-      'id',
-      products.product_id,
-      'brand',
-      products.brand,
-      'createdAt',
-      products.created_at,
-      'updatedAt',
-      products.updated
-    )
-  ) AS products
-FROM
-  stores
-  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
-  JOIN products ON stores_products.products_product_id = products.product_id
-WHERE
-  stores.store_id = '7956e435-df1b-465b-a052-6004ed14fe08'
-GROUP BY
-  stores.store_id;
-
--- Select all stores and return an array of products
-SELECT
-  stores.store_id,
-  stores.name,
-  stores.address,
-  stores.geog,
-  stores.created_at,
-  stores.updated_at,
-  ARRAY_AGG(
-    JSON_BUILD_OBJECT(
-      'name',
-      products.name,
-      'id',
-      products.product_id,
-      'brand',
-      products.brand,
-      'createdAt',
-      products.created_at,
-      'updatedAt',
-      products.updated
-    )
-  ) AS products
-FROM
-  stores
-  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
-  JOIN products ON stores_products.products_product_id = products.product_id
-GROUP BY
-  stores.store_id;
-
--- 
-SELECT
-  stores.store_id,
-  stores.name,
-  stores.address,
-  stores.geog,
-  stores.created_at,
-  stores.updated_at,
-  ARRAY_AGG(
-    JSON_BUILD_OBJECT(
-      'name',
-      products.name,
-      'id',
-      products.product_id,
-      'brand',
-      products.brand,
-      'createdAt',
-      products.created_at,
-      'updatedAt',
-      products.updated
-    )
-  ) AS products
-FROM
-  stores
-  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
-  JOIN products ON stores_products.products_product_id = products.product_id
-WHERE
-  products.reference = 'PAN'
-  AND ST_DWithin (
-    geog,
-    -- LONDON
-    ST_GeographyFromText ('POINT(-0.1275 51.50722)'),
-    -- METRES
-    5000
   )
 GROUP BY
   stores.store_id;
+
+-- Get store by ID with an array of products available
+SELECT
+  stores.store_id,
+  stores.name,
+  stores.address,
+  stores.geog,
+  stores.created_at,
+  stores.updated_at,
+  ARRAY_AGG(
+    JSON_BUILD_OBJECT(
+      'name',
+      products.reference,
+      'id',
+      products.product_id,
+      'brand',
+      products.brand,
+      'createdAt',
+      products.created_at,
+      'updatedAt',
+      products.updated
+    )
+  ) AS products
+FROM
+  stores
+  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
+  JOIN products ON stores_products.products_product_id = products.product_id
+WHERE
+  stores.store_id = '<STORE ID>'
+GROUP BY
+  stores.store_id
