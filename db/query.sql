cb2fc31be86736bf8fed1926b7587d73d8b1c490
Pedro Martin <pataruco@gmail.com>
2023-05-12 11:49:00 +0100

    Add query examples that return stores with a column products

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,4 @@
+-- Get stores within a radius from a point
 SELECT
   *
 FROM
@@ -11,6 +12,7 @@ WHERE
     10000
   );
 
+-- Get stores within a radius from a point and product name
 SELECT
   -- Avoid duplicates
   DISTINCT ON (store_id) stores.geog,
@@ -38,7 +40,7 @@ WHERE
     10000
   );
 
--- Get stores with products as JSON array
+-- Get store with products as JSON array
 SELECT
   stores.store_id,
   stores.name,
@@ -66,5 +68,65 @@ FROM
   JOIN products ON stores_products.stores_store_id = '<STORE_ID>'
 WHERE
   stores.store_id = '<STORE_ID>'
+GROUP BY
+  stores.store_id;
+
+-- Select store by ID and returns an array of products
+SELECT
+  stores.store_id,
+  stores.name,
+  stores.address,
+  stores.geog,
+  stores.created_at,
+  stores.updated_at,
+  ARRAY_AGG(
+    JSON_BUILD_OBJECT(
+      'name',
+      products.name,
+      'id',
+      products.product_id,
+      'brand',
+      products.brand,
+      'createdAt',
+      products.created_at,
+      'updatedAt',
+      products.updated
+    )
+  ) AS products
+FROM
+  stores
+  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
+  JOIN products ON stores_products.products_product_id = products.product_id
+WHERE
+  stores.store_id = '7956e435-df1b-465b-a052-6004ed14fe08'
+GROUP BY
+  stores.store_id;
+
+-- Select all stores and return an array of products
+SELECT
+  stores.store_id,
+  stores.name,
+  stores.address,
+  stores.geog,
+  stores.created_at,
+  stores.updated_at,
+  ARRAY_AGG(
+    JSON_BUILD_OBJECT(
+      'name',
+      products.name,
+      'id',
+      products.product_id,
+      'brand',
+      products.brand,
+      'createdAt',
+      products.created_at,
+      'updatedAt',
+      products.updated
+    )
+  ) AS products
+FROM
+  stores
+  JOIN stores_products ON stores_products.stores_store_id = stores.store_id
+  JOIN products ON stores_products.products_product_id = products.product_id
 GROUP BY
   stores.store_id;
